7efc6779092a70974764eddda61e80b9bb32bd68
Dave May <echo.dave@protonmail.com>
2019-11-25 22:03:59 -0500

    server loading and connectin to mysql

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
 require('dotenv').config();
+
+db = require('./database/models');
+const env = process.env.NODE_ENV || "development";
+//express server
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+//middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json);
+
+//static options
+let options = {
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['html'],
+    maxAge:'1d',
+    redirect:false,
+}
+
+
+//routes
+app.use(express.static(__dirname + '/public',options));
+
+app.use(function(req,res){
+res.status(404).sendFile(__dirname + "/public/404.html");
+});
+
+db.sequelize.sync().then(function(){
+app.listen(PORT,function () {
+    console.log("port " + PORT);
+    console.log("environment vars:")
+    console.log(env);
+});
+});
