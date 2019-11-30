346473e6788080a1c22928a29c2e4540b73dd4e9
Dave May <echo.dave@protonmail.com>
2019-11-29 21:10:21 -0500

    odering compares stock and returns messages where there isn't enough

--- public/js/api.js
+++ public/js/api.js
@@ -1,46 +1,57 @@
-$(document).ready(function() {
-  $.get("/api/products", function(data) {
-    console.log("-------");
-    console.log(data);
-    console.log("-------");
+$(document).ready(function () {
+    $.get("/api/products", function (data) {
+        console.log("-------");
+        console.log(data);
+        console.log("-------");
 
-    // console.log(results[0]);
+        // console.log(results[0]);
 
-    for (let i = 0; i < data.length; i++) {
-      console.log(data[i].product_name);
-      $("#products").append(`
-      <div class="columns is-centered" data-id='${data[i].id}'>
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i].product_name);
+            $("#products").append(`
+      <div class="columns is-centered is-multiline" data-id='${data[i].id}'>
       <div class="column is-one-fifth">
       ${data[i].product_name}</div>
       <div class="column is-one-fifth">
       ${data[i].department_name}
       </div>
       <div class="column is-one-fifth">
-      ${data[i].price}
-      </div
-      <div class="column is-narrow">
+      $${data[i].price}
+      </div>
+      <div class="column is-narrow quantity">
 <div class="control">
-<input type="number" class="input quantity" size="2" max-length="2" name="${data[i].product_name}"/>
+<input type="number" min="0" class="input quantity" size="2" max-length="2" value="0" name="${data[i].product_name}"/>
 </div>
       </div>
-    
-
       `);
-
-      /*    for (let [key, value] of Object.entries(data[i])) {
-        $(`.columns[data-id='${data[i].id}']`).append(`
-        <div class="column is-narrow">
-        ${key}: ${value}
-      </div>`);
-      } */
-    }
-  });
-
-  $("form").on("submit", function(event) {
-    event.preventDefault();
-    console.log($(this).serialize());
-    $.post("/api/submit", $("form").serialize(), function(order) {
-      console.log(order);
+        }
     });
-  });
+
+    $("form").on("submit", function (event) {
+        event.preventDefault();
+        let formData = $(this).find($("input"));
+        console.log("------");
+
+        let serializeArray = [];
+        for (let i = 0; i < formData.length; i++)
+            if (formData[i].value != 0) {
+                //console.log("formData");
+                //console.log(formData[i].value);
+                serializeArray.push(formData[i]);
+            }
+        console.log('array for post:');
+        console.log(serializeArray);
+
+        $.post("/api/submit", serializeArray, function (order) {
+            console.log("returned order");
+
+            console.log(order);
+            $('.column.stockCount').remove();
+            for (let i = 0; i < order.length; i++) {
+                $(`.columns[data-id=${order[i].id}]`).append(`<div class="column is-full stockCount is-centered">Sorry, not enough in stock: ${order[i].stock_quantity}</div>`)
+
+            }
+
+        });
+    });
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,8 @@
 const db = require("../database/models");
 
-module.exports = function(app) {
-  app.get("/api/products", function(req, res) {
+module.exports = function (app) {
+  //get data for front end display
+  app.get("/api/products", function (req, res) {
     db.Product.findAll({
       attributes: [
         "id",
@@ -10,13 +11,60 @@ module.exports = function(app) {
         "price"
         // ["stock_quantity", "Available"]
       ]
-    }).then(function(data) {
+    }).then(function (data) {
       res.json(data);
     });
   });
 
-  app.post("/api/submit", function(req, res) {
-    console.log(req);
-    res.json(req.body);
+  //post on order submission
+  app.post("/api/submit", function (req, res) {
+    console.log(req.body);
+    let order = req.body;
+
+    //convert returned object to array to loop through and build search attributes for databse
+    //compare quantities between order and database availability
+    order = Object.entries(order);
+    console.log('order:');
+    console.log(order);
+    let orderList = [];
+    for (let i = 0; i < order.length; i++) {
+      orderList.push(order[i][0]);
+      console.log("order list:");
+      console.log(orderList);
+    }
+
+    db.Product.findAll({
+      where: {
+        product_name: [orderList]
+      }
+    }).then(function (data) {
+      console.log('then order list:');
+      console.log(orderList);
+
+      console.log('data');
+      console.log(data);
+
+      //pushing low stock items to arrray - stockLevels
+      let stockLevels = [];
+      for (let i = 0; i < orderList.length; i++) {
+        if (data[i].stock_quantity >= order[i][1]) {
+          // stockLevels.push(data[i].product_name + " stock good");
+        } else {
+          stockLevels.push(data[i].product_name);
+        }
+      }
+
+      //query database for stock levels of the low items for displaying on frontend. 
+      //Using id to match columns id attribute
+      db.Product.findAll({
+        attributes: ['id', 'stock_quantity'],
+        where: {
+          product_name: stockLevels
+        }
+      }).then(function (data) {
+        res.json(data);
+
+      })
+    });
   });
 };
