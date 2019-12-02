46a7d5b904ec725ef6d00b0232b6ea5c2b676c68
Dave May <echo.dave@protonmail.com>
2019-12-02 09:48:30 -0500

    expanded price range in database and model

--- database/models/product.js
+++ database/models/product.js
@@ -1,21 +1,21 @@
-module.exports = function(sequelize,dataTypes){
-const Product = sequelize.define('Product',{
-    product_name: {
-        type:dataTypes.STRING,
-        len:[2,100]
-    },
-    department_name:{
-        type:dataTypes.STRING,
-        len:[2,30]
-    },
-    price:{
-        type:dataTypes.DECIMAL(5,2),
-        allowNull:false
-    },
-    stock_quantity:{
-        type:dataTypes.INTEGER,
-        defaultValue:0
-    }
-});
-return Product
+module.exports = function (sequelize, dataTypes) {
+    const Product = sequelize.define('Product', {
+        product_name: {
+            type: dataTypes.STRING,
+            len: [2, 100]
+        },
+        department_name: {
+            type: dataTypes.STRING,
+            len: [2, 30]
+        },
+        price: {
+            type: dataTypes.DECIMAL(8, 2),
+            allowNull: false
+        },
+        stock_quantity: {
+            type: dataTypes.INTEGER,
+            defaultValue: 0
+        }
+    });
+    return Product
 }
